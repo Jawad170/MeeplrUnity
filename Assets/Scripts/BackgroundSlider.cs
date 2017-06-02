49aaf657c4ec1967ac10cf1ada07526e476d6c80
Jawad J R <Jawad_Jandaly@hotmail.com>
2017-06-02 11:29:31 -0300

    feat(BGSlider): Random BGs
    The background can now choose between a bunch of backgrounds each time the app starts. For now it's a few random background images just for testing.
    Also made sure that the image stays consistent throughout the app during one session.
    
    Scene 04 - Home Page: worked on it a little and added the bottom buttons. It still needs lots of work.
    
    Organized graphics and materials into appropriate folders.

--- Assets/Scripts/BackgroundSlider.cs
+++ Assets/Scripts/BackgroundSlider.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class BackgroundSlider : MonoBehaviour {
 
-	public float xStart = 1.0f ;
-	public float xEnd   = 9.0f ;
-	public float Speed  = 1.0f ;
+	[Header("Control Parameters")	]
+	public float 		xStart 		= 1.0f 	;
+	public float 		xEnd   		= 9.0f 	;
+	public float 		Speed  		= 1.0f 	;
+	[Header("Possible Backgrounds")	]
+	public Material[] 	Mats	   			;
+	[Header("Development Tools")	]
+	public bool 		DebugMode 	= false	;
 
-	private bool rev    = false;
+	private bool 		rev    		= false ;
+
+	public static bool 	BGChosen 	= false	;
 
 	void Start()
 	{
+		if (!BGChosen) ChooseMat ();
+		else 		   SetMat    ();
+
 		if ( PlayerPrefs.GetFloat("BGSlideX") == 0.0f ) 
 			transform.position = new Vector3 (xStart, transform.position.y, transform.position.z);
 		else
@@ -37,5 +47,27 @@ public class BackgroundSlider : MonoBehaviour {
 		}
 
 		PlayerPrefs.SetFloat ("BGSlideX", transform.position.x);
+	}
+
+	public void ChooseMat()
+	{
+		if (DebugMode) Debug.Log ("BackgroundSlider: BG Mat NOT selected, Rolling . . .");
+
+		int Choice = (int) Random.Range (0, Mats.Length);
+
+		gameObject.GetComponent<MeshRenderer> ().material = Mats [Choice];
+
+		BackgroundSlider.BGChosen = true;
+
+		PlayerPrefs.SetInt ("BGMat", Choice);
+
+		if (DebugMode) Debug.Log ("BackgroundSlider: New BG Mat selected, setting [" + PlayerPrefs.GetInt ("BGMat") + "]");
+	}
+
+	public void SetMat()
+	{
+		if (DebugMode) Debug.Log ("BackgroundSlider: BG Mat already selected, setting [" + PlayerPrefs.GetInt ("BGMat") + "]");
+
+		gameObject.GetComponent<MeshRenderer> ().material = Mats [PlayerPrefs.GetInt("BGMat")];
 	}
 }
